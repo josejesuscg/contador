857df3e5c734874724fca917560a0b8f3e5cad26
josejesuscg <josejesuscg08@gmail.com>
2021-07-20 19:19:04 -0400

    proyecto finalizado

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:contador/src/app.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+//Nosotros escribimos
+// import 'src/app.dart';
+
+void main() {
+  runApp(new MyApp());
+}
